3a9d35ab3d9f22c58d6b0252b09395f52f4b7b8c
Sean Peng <seanpeng11@gmail.com>
2023-02-15 17:16:01 -0500

    Refactor choose_questions method

--- question_extraction/app.py
+++ question_extraction/app.py
@@ -19,52 +19,17 @@ if (myDb.is_connected()):
 else:
     print("Not connected")
 
+
 @app.route('/choose_questions', methods=['GET', 'POST'])
 def choose_questions():
     try:
         if request.method == 'POST':
-            print(request.form)
             # fetch form data
-            if request.form['submit_button'] == '1':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter1' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '2':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter2' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '3':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter3' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '4':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter4' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '5':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter5' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '6':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter6' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '7':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter7' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '8':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter8' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '9':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter9' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '10':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter10' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '11':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter11' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            elif request.form['submit_button'] == '12':
-                query = "select question from questions where status = 'publish'  and category = 'Chapter12' ORDER by RAND() LIMIT 1"
-                return redirect(url_for('display_questions', query=query))
-            
-            
+            query = f"select question from questions where status = 'publish'  and category = 'Chapter{request.form['submit_button']}' ORDER by RAND() LIMIT 1"
+            return redirect(url_for('display_questions', query=query))
+
     except mysql.connector.Error as error:
-            return "Failed to create due to this error: " + repr(error)
+        return "Failed to create due to this error: " + repr(error)
 
     return render_template('choose_questions.html')
 
@@ -75,7 +40,6 @@ def display_questions(query):
     try:
         cursor.execute(query)
         res = cursor.fetchall()
-
 
         if request.method == 'POST':
             # fetch form data
@@ -88,6 +52,7 @@ def display_questions(query):
         return "Failed to create due to this error: " + repr(error)
 
     return render_template('display_questions.html', res=res)
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True)
