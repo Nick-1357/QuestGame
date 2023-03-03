a86069006b539f7885ca8c3be2de4b9c6b904bd7
reesewang25 <reesewang10@gmail.com>
2023-03-02 23:10:21 -0500

    added timeLeft

--- quest-frontend/src/components/timeLeft.tsx
+++ quest-frontend/src/components/timeLeft.tsx
@@ -4,7 +4,7 @@ import { IGlobalState } from "../store/reducers";
 import {Box} from '@chakra-ui/react'
 
 const TimeLeft = () => {
-    const time = useSelector((state: IGlobalState) => state.time);
+    const time = useSelector((state: IGlobalState) => state.timeLeft);
     return(
         <Box bg= "tomato"> Time left: {time}</Box>
     );
