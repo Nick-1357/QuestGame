5a9b67a1fb12ee6016dfe3bdfcb30a79b6c4ced5
JY <jchuah8@gatech.edu>
2023-03-02 19:44:14 -0500

    Corrected import name

---
+++ quest-frontend/src/store/index.tsx
@@ -0,0 +1,12 @@
+import {configureStore, applyMiddleware, getDefaultMiddleware} from "@reduxjs/toolkit";
+import createSagaMiddleware from "@redux-saga/core";
+import gameReducer from "./reducers";
+import watcherSagas from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = configureStore({reducer: gameReducer, middleware: [sagaMiddleware]});
+
+sagaMiddleware.run(watcherSagas);
+
+
+export default store;
