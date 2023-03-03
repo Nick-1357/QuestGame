a714299d6e10fb790b6b6eb386029430f902cc71
reesewang25 <reesewang10@gmail.com>
2023-03-02 23:06:02 -0500

    added scoreboard, timer, question components

--- quest-frontend/src/App.tsx
+++ quest-frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { ChakraProvider, Container, Grid, GridItem, Heading } from '@chakra-ui/r
 import { Provider } from 'react-redux';
 import store from "./store"
 import GameBoard from './components/gameBoard';
+import ScoreBoard from './components/scoreBoard';
+import TimeLeft from './components/timeLeft';
 
 function App() {
   return (
@@ -12,6 +14,10 @@ function App() {
           <Heading as ="h1"
           size="x1">
             Quest Game
+            <Grid templateColumns='repeat (2,1fr)' gap={6}>
+              <GridItem> <ScoreBoard /> </GridItem>
+              <GridItem> <TimeLeft /> </GridItem>
+            </Grid>
             <GameBoard height={600} width={500}/>
           </Heading>
         </Container>
@@ -21,54 +27,5 @@ function App() {
 }
 
 
-// const Board = ({ height, width }) => {
-//   return (
-//     <div className='board' style={{
-//       border: "3px solid black",
-//     }}>
-//       <div className='header'>
-//         <ScoreBoard score={100}/>
-//         <Timer time = {2} />
-//       </div>
-//       <div className='gameboard'>
-//         <gameBoard height={height} width = {width}/>
-//       </div>
-//     </div>
-//   );
-// };
-
-// const GameBoard = ({ height, width }) => {
-  
-//   return (
-//     <canvas
-//       style={{
-//         border: "3px solid black",
-//       }}
-//       height={height}
-//       width={width}
-//     />
-//   );
-// };
-
-// const ScoreBoard = ({score}) => {
-//   return (
-//     <div style={{
-//       border: "1px solid black",
-//       width:"50px"
-//     }}> Score: {score} </div>
-//   );
-// }
-
-// const Timer = ({time}) => {
-//   return (
-//     <div style={{
-//       border: "1px solid black",
-//       width: "50px"
-//     }}> Timer: {time} </div>
-//   )
-// }
-
-
-//const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
 export default App;
---
+++ quest-frontend/src/components/Question.tsx
@@ -0,0 +1,13 @@
+import Popup from 'reactjs-popup'
+
+const Question = props => {
+    return (<Popup trigger = {} >
+        <p>{props.question}</p>
+        <button> Generate hint</button>
+        <button> Submit answer</button>
+    </Popup>
+    //trigger will be answering the question right
+    );
+}
+
+export default Question;
